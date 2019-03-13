fad2cf1a30382c37ba9feaaaefd821605cf7f676
Corwin Voss <mason.cvoss.2019@gmail.com>
2019-03-13 12:19:19 -0400

    Almost finished AdvArrayList2

--- AdvArrayList2/src/com/company/Main.java
+++ AdvArrayList2/src/com/company/Main.java
@@ -12,25 +12,36 @@ public class Main {
         String password = " ";
         ArrayList<String> usernames = new ArrayList<>();
         ArrayList<String> passwords = new ArrayList<>();
-        System.out.println("Would you like to login or create an account?");
+
         while (true) {
-            if (scanner.nextLine().equalsIgnoreCase("login")) {
-                System.out.println("Enter Username: ");
-                System.out.println("Enter Password: ");
-                for (int i = usernames.size(); i <= 0; i--){
-                    if (username.equalsIgnoreCase(usernames.get(i))){
-                        break;
-                    }
-                }
-            } else if (scanner.nextLine().equalsIgnoreCase("create account")) {
+            System.out.println("Would you like to login or create an account?");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("login")) {
                 System.out.println("Enter Username: ");
                 username = scanner.nextLine();
                 System.out.println("Enter Password: ");
                 password = scanner.nextLine();
-
+                for (int i = usernames.size() - 1; i >= 0; i--) {
+                    if (usernames.get(i).equalsIgnoreCase(username)) {
+                        if (passwords.get(i).equalsIgnoreCase(password)) {
+                            System.out.println("Welcome, " + username);
+                            System.exit(0);
+                        } else {
+                            System.out.println("Sorry, the account does not match records");
+                        }
+                    }
+                }
+            } else if (input.equalsIgnoreCase("create account") || input.equalsIgnoreCase("create an account") || input.equalsIgnoreCase("create")) {
+                System.out.println("Enter Username: ");
+                username = scanner.nextLine();
+                System.out.println("Enter Password: ");
+                password = scanner.nextLine();
                 usernames.add(username);
                 passwords.add(password);
+
             }
         }
     }
 }
+
+
