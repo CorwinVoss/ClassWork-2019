043e915f713fcbbac55b435abe63b6bec5adc8a9
Corwin Voss <mason.cvoss.2019@gmail.com>
2019-03-13 12:42:01 -0400

    Finished AdvArrayList2

--- AdvArrayList2/src/com/company/Main.java
+++ AdvArrayList2/src/com/company/Main.java
@@ -10,38 +10,49 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String username = " ";
         String password = " ";
+        boolean check = true;
         ArrayList<String> usernames = new ArrayList<>();
         ArrayList<String> passwords = new ArrayList<>();
-
         while (true) {
-            System.out.println("Would you like to login or create an account?");
-            String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("login")) {
-                System.out.println("Enter Username: ");
-                username = scanner.nextLine();
-                System.out.println("Enter Password: ");
-                password = scanner.nextLine();
-                for (int i = usernames.size() - 1; i >= 0; i--) {
-                    if (usernames.get(i).equalsIgnoreCase(username)) {
-                        if (passwords.get(i).equalsIgnoreCase(password)) {
-                            System.out.println("Welcome, " + username);
-                            System.exit(0);
+            while (true) {
+                System.out.println("Would you like to login or create an account?");
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("login")) {
+                    System.out.println("Enter Username: ");
+                    username = scanner.nextLine();
+                    System.out.println("Enter Password: ");
+                    password = scanner.nextLine();
+                    for (int i = usernames.size() - 1; i >= 0; i--) {
+                        if (usernames.get(i).equalsIgnoreCase(username) && passwords.get(i).equalsIgnoreCase(password)) {
+                                System.out.println("Welcome, " + username);
+                                System.exit(0);
+                            } else {
+                                System.out.println("Sorry, the account does not match records");
+                            }
+                        }
+                } else if (input.equalsIgnoreCase("create account") || input.equalsIgnoreCase("create an account") || input.equalsIgnoreCase("create")) {
+                    System.out.println("Enter Username: ");
+                    username = scanner.nextLine();
+                    for (int i = usernames.size() - 1; i >= 0; i--) {
+                        if (usernames.get(i).equals(username) && usernames.size() > 0) {
+                            System.out.println("This account already exists");
+                            check = false;
+                            break;
                         } else {
-                            System.out.println("Sorry, the account does not match records");
+                            check = true;
                         }
                     }
-                }
-            } else if (input.equalsIgnoreCase("create account") || input.equalsIgnoreCase("create an account") || input.equalsIgnoreCase("create")) {
-                System.out.println("Enter Username: ");
-                username = scanner.nextLine();
-                System.out.println("Enter Password: ");
-                password = scanner.nextLine();
-                usernames.add(username);
-                passwords.add(password);
-
+                    if (!check){
+                        break;
+                    }
+                    if (usernames.size() == 0) {
+                        usernames.add(username);
+                    }
+                        System.out.println("Enter Password: ");
+                        password = scanner.nextLine();
+                        passwords.add(password);
+                    }
+                    }
             }
         }
     }
-}
-
-
